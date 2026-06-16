1a6c79a78b2d88ab77efe3610ff26966316d7601
FatmaNasser29 <fatma.nasser1213@gmail.com>
2026-06-16 20:10:10 +0300

    add color app pallet

---
+++ lib/config/theme/app_colors_pallet.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColorsPallet {
+  static const Color assentsRed = Color(0xFFE11D48);
+  static const Color assentsOrange = Color(0xFFCF7800);
+  static const Color assentsgreen = Color(0xFF1B8A48);
+  static const Color assentsIndigo = Color(0xFF6D27EE);
+  static const Color backgroundSecondary = Color(0xFFACAABF);
+  static const Color assentsBlack = Color(0xFF101828);
+  static const Color assentsBlue = Color(0xFF3686FF);
+  static const Color babyBlue = Color(0xFF00C4DA);
+       
+}
