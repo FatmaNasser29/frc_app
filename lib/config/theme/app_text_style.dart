8c33f118984e691db30c81c6cc77c900b3029460
FatmaNasser29 <fatma.nasser1213@gmail.com>
2026-06-16 19:48:49 +0300

    add text style

--- lib/config/theme/app_text_style.dart
+++ lib/config/theme/app_text_style.dart
@@ -1,3 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; 
+
 class AppTextStyle {
-  
+  AppTextStyle.internal();
+  final textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: GoogleFonts.funnelDisplay().fontFamily,
+  );
+
+ final textStyle124 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    fontFamily: GoogleFonts.funnelDisplay().fontFamily,
+  );
+
 }
