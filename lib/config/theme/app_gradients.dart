ebd657c066a0a5aade2c908bae3fd89374d0f952
FatmaNasser29 <fatma.nasser1213@gmail.com>
2026-06-17 13:14:55 +0300

    create app gradients class

---
+++ lib/config/theme/app_gradients.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppGradients {
+  
+  static const LinearGradient primaryGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color(0xFF00E3B5),
+      Color(0xFF00C4DA),
+      Color(0xFF3686FF),
+      Color(0xFF7419EB),
+    ],
+  );
+}
