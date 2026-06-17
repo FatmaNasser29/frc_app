39f1d0fe24ccbe5de7fe1ab690e401f62e3313e7
FatmaNasser29 <fatma.nasser1213@gmail.com>
2026-06-17 14:04:16 +0300

    implement on boarding data

---
+++ lib/features/onboarding/data/data_sources/on_boarding_data.dart
@@ -0,0 +1,31 @@
+
+import 'package:frc_app/features/onboarding/data/models/on_boarding_models.dart';
+
+class OnboardingData {
+  static const List<OnboardingModel> pages = [
+    OnboardingModel(
+      image: 'assets/images/on_boarding_one.png',
+      title: 'Join Your Professional Network',
+      description:
+          'Connect with professionals and companies in your industry.Build your profile, grow your network, and discover real opportunities.',
+    ),
+    OnboardingModel(
+      image: 'assets/images/on_boarding_two.png',
+      title: 'Grow Your Career or Your Business',
+      description:
+          'For Professionals.\nFind the right job, develop your skills, and grow your professional network.',
+    ),
+    OnboardingModel(
+      image: 'assets/images/on_boarding_three.png',
+      title: 'Grow Your Career or Your Business',
+      description:
+          'For Businesses.\nHire talent, expand your market, and connect with suppliers and industry services.',
+    ),
+    OnboardingModel(
+      image: 'assets/images/on_boarding_four.png',
+      title: 'Smart Services for Your Industry',
+      description:
+          'Access marketplaces, consulting, analytics, and AI-powered insights to grow and scale your business.',
+    ),
+  ];
+}
